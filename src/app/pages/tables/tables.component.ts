997dee075ed798b47d239e59552217730d11c8dd
Rafael Medina <rafael.medina46005@ucaldas.edu.co>
2024-12-18 08:53:26 -0500

    Implementacion de tablas

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tables',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablesComponent implements OnInit {
 
-  constructor() { }
+ // Arreglo con los módulos cargados dinámicamente
+ routes = [
+  'administradores',
+  'categorias',
+  'categoriasProductos',
+  'centrosDistribucion',
+  'clientes',
+  'conductores',
+  'contratos',
+  'cuotas',
+  'departamentos',
+  'direcciones',
+  'dirListaOrdenes',
+  'duenos',
+  'duenosVehiculos',
+  'empresas',
+  'facturas',
+  'gastos',
+  'hoteles',
+  'lotes',
+  'municipios',
+  'operaciones',
+  'personasNaturales',
+  'productos',
+  'restaurantes',
+  'rutas',
+  'seguros',
+  'servicios',
+  'turnos',
+  'vehiculos',
+  'vehiculosConductores',
+  'usuarios',
+];
 
-  ngOnInit() {
-  }
+constructor(private router: Router) {}
+
+// Método para redirigir a la ruta seleccionada
+navigateTo(route: string, action: string) {
+  this.router.navigate([`${route}/${action}`]);
+}
+
+// Hook de inicialización
+ngOnInit(): void {
+  console.log('TablesComponent inicializado');
+  // Puedes agregar lógica aquí si necesitas inicializar datos o hacer verificaciones.
+}
+
 
 }
